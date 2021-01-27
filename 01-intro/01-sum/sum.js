265bc99009e2b55b8176873e2f0aa1368fb67092
Gregory Tereshko <gtrsh@users.noreply.github.com>
2021-01-27 04:56:54 +0300

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
-function sum(a, b) {
-  /* ваш код */
-}
+const isNumber = (num) => typeof(num) === "number";
+const sum = (a, b) => {
+    if (isNumber(a) && isNumber(b)) {
+        return a + b;
+    } else {
+        throw new TypeError();
+    }
+};
 
 module.exports = sum;
